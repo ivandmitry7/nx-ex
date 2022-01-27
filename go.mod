89e940af98f1fccbe8869f24754b7ca08c5c4fab
xokc <xokc@mail.ru>
2022-01-27 20:48:48 +0300

    Add tests for Result.Commit/makeID/PushCoords

--- go.mod
+++ go.mod
@@ -3,5 +3,9 @@ module github.com/o-kos/nx-ex
 go 1.17
 
 require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- internal/result/result.go
+++ internal/result/result.go
@@ -1,5 +1,14 @@
 package result
 
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
 type Date struct {
 	Beg int64 `json:"beg"`
 	End int64 `json:"end"`
@@ -8,12 +17,14 @@ type Date struct {
 type Coords struct {
 	Lat float32 `json:"lat"`
 	Lon float32 `json:"lon"`
+	raw string
 }
 
 type Area struct {
 	Type   string   `json:"type"`
 	Coords []Coords `json:"coords"`
 	Radius float32  `json:"radius"`
+	raw    string
 }
 
 type Result struct {
@@ -22,4 +33,51 @@ type Result struct {
 	Reason string `json:"reason"`
 	Date   Date   `json:"date"`
 	Area   []Area `json:"area"`
+}
+
+func (r *Result) makeID() string {
+	as := ""
+	for _, a := range r.Area {
+		as = as + fmt.Sprintf("[%s], ", a.raw)
+	}
+	as = strings.TrimSuffix(as, ", ")
+
+	id := fmt.Sprintf(
+		"%s#%s:%s...%s[%s]",
+		r.Source, r.Reason,
+		time.Unix(r.Date.Beg, 0).UTC().Format(time.RFC3339),
+		time.Unix(r.Date.End, 0).UTC().Format(time.RFC3339),
+		as,
+	)
+
+	return id
+}
+
+func (r *Result) PushCoords(radius string, coords []string) {
+	a := Area{}
+	if radius == "" {
+		a.Radius = 0
+		a.Type = "polygon"
+	} else {
+		a.Type = "circle"
+		if ml, err := strconv.ParseFloat(radius, 32); err == nil {
+			a.Radius = float32(ml * 1.852)
+		} else {
+			fmt.Printf("unable to convert radius value %q\n", radius)
+			a.Radius = 0
+		}
+	}
+
+	cs := ""
+	for _, c := range coords {
+		a.Coords = append(a.Coords, Coords{raw: c})
+		cs += fmt.Sprintf("[%s], ", c)
+	}
+	a.raw = fmt.Sprintf("%s/%s[%s]", a.Type, radius, strings.TrimSuffix(cs, ", "))
+	r.Area = append(r.Area, a)
+}
+
+func (r *Result) Commit() {
+	hash := md5.Sum([]byte(r.makeID()))
+	r.Hash = hex.EncodeToString(hash[:])
 }
